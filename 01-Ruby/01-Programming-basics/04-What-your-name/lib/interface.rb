2c83830b4eadfa3d38804a784c8441d26f3469ba
Julien Debray <julien.debray.pro@gmail.com>
2014-10-07 14:36:44 +0200

    user requests

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,5 +1,18 @@
 require_relative "compute_name"
 
+puts "First name?"
+
+first_name = gets.chomp
+
+puts "Middle name?"
+
+middle_name = gets.chomp
+
+puts "Last name?"
+
+last_name = gets.chomp
+
+print "Hello, #{compute_name(first_name, middle_name, last_name)}!!!"
 # TODO: ask for the first name
 # TODO: ask for the middle name
 # TODO: ask for the last name
