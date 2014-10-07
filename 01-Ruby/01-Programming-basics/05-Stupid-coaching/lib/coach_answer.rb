c7c21853afd0876579694e2e54fd29300c79bb04
Julien Debray <julien.debray.pro@gmail.com>
2014-10-07 17:39:47 +0200

    Coach done

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -12,10 +12,10 @@ end
 def coach_answer_enhanced(your_message)
   if your_message == "I AM GOING TO WORK RIGHT NOW!"
     return ""
-  elsif your_message == your_message.upcase
-    return "I can feel your motivation! I don't care, get dressed and go to work!"
   elsif (your_message == your_message.upcase) && (your_message.split("").last == "?")
     return "I can feel your motivation! Silly question, get dressed and go to work!"
+  elsif your_message == your_message.upcase
+    return "I can feel your motivation! I don't care, get dressed and go to work!"
   else
     return coach_answer(your_message)
   end
