357351a7253821e51b8b06fb2531b1292e2fdb91
Julien Debray <julien.debray.pro@gmail.com>
2014-10-08 16:14:35 +0200

    black jack2

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -22,18 +22,22 @@ end
 def play_game
   # TODO: make the user play from terminal in a while loop that will stop
   #       when the user will not be asking for  a new card
+
   player_score = 0
   bank_score = pick_bank_score
   answer = "y"
+
   while (player_score < 22) && ((answer == "y") || (answer == "yes"))
     puts "Card ? (type 'y' or 'yes' for a new card)"
     answer = gets.chomp
+
     if (answer == "y") || (answer == "yes")
       player_score = pick_player_card + player_score
       puts state_of_the_game(player_score, bank_score)
     else
       puts state_of_the_game(player_score, bank_score)
     end
+
   end
   puts end_game_message(player_score, bank_score)
 end
