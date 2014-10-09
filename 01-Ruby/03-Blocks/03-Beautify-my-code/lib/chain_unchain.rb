546891d6b1d7ac372491837c7605b191a94d9297
Julien Debray <julien.debray.pro@gmail.com>
2014-10-09 15:08:34 +0200

    done2

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,18 +1,28 @@
 def shuffle_word(a_word)
   # TODO: refactor this method
-
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-
-  shuffle_array
-
+  a_word.upcase.chars.shuffle
 end
 
 
 def quote_prime_numbers(n)
   # TODO: refactor this method
-
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  tab_premier = (1..n).select do |i|
+    (2..i - 1).select { |k| i % k == 0 }.count == 0
+  end
+  tab_premier.map { |prime_num| "#{prime_num} is prime" }
 end
+
+
+# tab_prem = (1..n).reject do |i|
+#   non_premier = false
+#   (2..i - 1).each do |k|
+#     premier = true if i % k == 0
+#   end
+#   non_premier
+# end
+# tab_prem.map { |prime_num| "#{prime_num} is prime" }
+# end
+
+
+
+# (1..n).select { |i| (2..i - 1).select { |k| i % k == 0 }.count == 0 }.map { |prime_num| "#{prime_num} is prime" }
