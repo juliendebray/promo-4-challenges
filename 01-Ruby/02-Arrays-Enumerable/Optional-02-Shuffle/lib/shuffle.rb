cae4682fd292dd16926ee00762332eff35cb68d6
Julien Debray <julien.debray.pro@gmail.com>
2014-10-10 09:03:16 +0200

    shuffle ok

--- 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
@@ -1,38 +1,12 @@
 def shuffle(array)
   # TODO: shuffle the array manually
-  l = array.length
   shuff = []
-  puts shuff
-  if shuff.size == 0
-    puts "shuff vide"
-  end
-  i = 0
-  k = 0
-
-  for k in (0..l-1)
-    while !shuff.include?(i)
-      i = rand(0..l-1)
-      shuff[k] = i
+  i = rand(0..array.length - 1)
+  (0..array.length - 1).each do |k|
+    while shuff.include?(array[i])
+      i = rand(0..array.length - 1)
     end
-    k = k + 1
+    shuff[k] = array[i]
   end
   shuff
 end
-
-
-tab = ["ju", "flo", "ro", "max", "etc"]
-alea = []
-puts tab
-l = tab.length
-puts l
-i = 0
-for k in (0..l-1) do
-  puts "k = #{k}"
-  until !(alea.include?(i))
-  i = rand(0..l-1)
-  puts "i dans while : #{i}"
-  alea << i
-  puts "alea en cours: #{alea}"
-  end
-end
-puts "alea final #{alea}"
--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,34 @@
 def beer_song(beer_start_count)
   # TODO: sing the song
+  case beer_start_count
+  when 1
+    song = "1 bottle of beer on the wall, 1 bottle of beer!\n\
+Take one down, pass it around, no more bottles of beer on the wall!"
+  when 2
+    song = "2 bottles of beer on the wall, 2 bottles of beer!\n\
+Take one down, pass it around, 1 bottles of beer on the wall!\n1 bottle of beer on the wall, 1 bottle of beer!\n\
+Take one down, pass it around, no more bottles of beer on the wall!"
+  else
+  song = "#{beer_start_count} bottles of beer on the wall, #{beer_start_count} bottles of beer!\n"
+  (beer_start_count - 1).downto(2) do |i|
+    song = song + "Take one down, pass it around, #{i} bottles of beer on the wall!\n\
+#{i} bottles of beer on the wall, #{i} bottles of beer!\n"
+    end
+  song = song + "Take one down, pass it around, 1 bottle of beer on the wall!\n\
+1 bottle of beer on the wall, 1 bottle of beer!\n\
+Take one down, pass it around, no more bottles of beer on the wall!"
+  end
 end
+
+
+puts beer_song(1)
+puts " "
+
+puts beer_song(2)
+puts " "
+
+puts beer_song(3)
+puts " "
+
+puts beer_song(4)
+puts " "
