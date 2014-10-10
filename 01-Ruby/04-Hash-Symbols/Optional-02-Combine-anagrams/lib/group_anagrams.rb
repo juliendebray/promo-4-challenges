e309ca60c54fb473bbb03d8b88113a01025c6f7d
Julien Debray <julien.debray.pro@gmail.com>
2014-10-10 18:02:27 +0200

    anagrams

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,8 +1,24 @@
+def anag?(word_1,word_2)
+  word_1.split("").sort == word_2.split("").sort
+end
+
+
+#puts anag?("cats", "rasc")
+
+
 def group_anagrams(words)
   #TODO: group anagrams
+  tab = []
+  words.each_with_index do |mot_1, ind|
+    tab[ind] = words.select {|mot_2| anag?(mot_1, mot_2)}
+  end
+  tab.uniq
 end
 
 # input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
 # =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
+
+
+
