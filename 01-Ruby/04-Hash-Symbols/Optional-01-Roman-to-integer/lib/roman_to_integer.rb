4f4138934d10a7c159859779ad6e10a9cd55bee9
Julien Debray <julien.debray.pro@gmail.com>
2014-10-10 16:42:38 +0200

    roman_number

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,9 +1,29 @@
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+  roman_tr ={
+    "I" => 1,
+    "V" => 5,
+    "X" => 10
+  }
+  ctrl = ["na","I","V","X"]
+  sum = 0
+  tab = roman_string.split("")
+  tab = tab << "na"
+
+  (0..tab.length - 2).each do |i|
+    if ctrl.index(tab[i]) >= ctrl.index(tab[i + 1])
+      sum += roman_tr[tab[i]]
+    elsif ctrl.index(tab[i]) < ctrl.index(tab[i + 1])
+      sum += - roman_tr[tab[i]]
+    else
+      puts "Error"
+    end
+  end
+  sum
 end
 
 # testing your program
-puts roman_to_integer('IV') == 4 # => true
-puts roman_to_integer('XII') == 12 # => true
-puts roman_to_integer('XIX') == 19 # => true
+puts roman_to_integer('IV')
+puts roman_to_integer('XII')
+puts roman_to_integer('XIX')
 
