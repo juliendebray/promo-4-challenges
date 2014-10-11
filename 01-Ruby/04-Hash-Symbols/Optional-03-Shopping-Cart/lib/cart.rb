65082f455e53576bd42095f3ca0955454e9422d1
Julien Debray <julien.debray.pro@gmail.com>
2014-10-11 10:11:20 +0200

    shopping done

--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
@@ -3,13 +3,44 @@
 
 def add_to_cart(cart, product)
   # TODO: you get a cart and a product. Add the product to the cart!
+  #cart = []
+  cart << product
 end
+
+# cart = ["a"]
+# p cart
+# p add_to_cart(cart,"banane")
+# p cart
 
 def cart_to_s(cart)
   # TODO: you get a cart, return a coma-separated String list of the contained products
+  hash_count = {}
+  cart.each {|prod| hash_count[prod] = cart.count(prod) }
+  phr = ""
+  hash_count.each do |prod, val|
+    if val == 1
+      phr += "#{prod}, "
+    else
+      phr += "#{prod} x #{val}, "
+    end
+  end
+  return phr[0..-3]
+  #cart.join(", ")
 end
+
+# cart = ["a","b","c","a", "d", "a", "c", "d"]
+# p cart
+# p cart_to_s(cart)
+
+# p cart_to_s(cart)
+
 
 def cart_total_price(cart, store_items)
   # TODO: you get a cart and the store items with their prices. Return the total
   # price of the shopping cart
+  sum = 0
+  cart.each do |value|
+    sum = sum + store_items[value.to_sym] if store_items[value.to_sym] != nil
+  end
+  sum
 end
--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
@@ -11,17 +11,26 @@ def store_items
 
   # TODO: find the correct data structure to hold the shopping store items
   # instead of using nil below
-  store = nil
+  store = {
+    yogurts: 2,
+    meat: 7,
+    vegetables: 5,
+    potatoes: 2,
+    rice: 1
+  }
 
   return store
 end
+#p store_items
 
 def is_product_in_store?(product)
   # TODO: Check whether product is present in the store items
+  return store_items.has_key?(product.to_sym)
 end
 
 def price_of_product(product)
   # TODO: Return the price of the given product, if it's in store. nil otherwise
+  return store_items[product.to_sym]
 end
 
 def store_items_to_s
@@ -31,4 +40,10 @@ def store_items_to_s
   # ...
   #
   # TIP: add line breaks in your String with "\n"
+  phrase = ""
+  store_items.each do |key, value|
+   phrase = phrase + "- #{key.to_s}: #{value}€\n"
+  end
+  phrase
 end
+
