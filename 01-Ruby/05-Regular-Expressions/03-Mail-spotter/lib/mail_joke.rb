b20d1f7ea2077640f5e91a554448204896915727
Julien Debray <julien.debray.pro@gmail.com>
2014-10-13 14:51:29 +0200

    email ok

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,5 +1,37 @@
 # Encoding: utf-8
 
+def email_user(email)
+  return email.match(/.+@/).to_s[0...- 1].gsub(".", " ")
+end
+
+def email_domain(email)
+  return email.match(/@.+/).to_s[1..- 1].gsub(".", " ")
+end
+
+# p email_user("boris.dupon@lew.fr")
+# p email_domain("boris.dupon@lew.fr")
+
+
 def mail_joke(email)
   # TODO: Return (not print!) a joke suited to the email provided
+  if (email.include? "@") && (email.include? ".")
+    case email_domain(email).downcase
+    when "lewagon org"
+      return "Well done #{email_user(email)}, you're skilled and capable"
+    when "gmail com"
+      return "#{email_user(email)}, you're an average but modern person"
+    when "live com"
+      return "#{email_user(email)}, aren't you born before 1973?"
+    else
+      return "Sorry #{email_user(email)}, we don't know how to judge '#{email_domain(email).gsub(" ", ".")}'"
+    end
+  else
+    fail ArgumentError, "Email is not valid"
+  end
 end
+
+p mail_joke("boris.nom@live.com")
+
+
+
+
