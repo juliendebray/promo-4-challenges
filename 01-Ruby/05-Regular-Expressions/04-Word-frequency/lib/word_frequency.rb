3744e86d5148beb67ca6790a3f5c6ef97506adaa
Julien Debray <julien.debray.pro@gmail.com>
2014-10-13 16:52:49 +0200

    words counter ok

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,27 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
   # TODO: return hash of occurences of number_of_word most frequent words
+  string = ""
+  File.open(file_name, "r").each_line do |line|
+    string += " " + line
+  end
+  tab_all = string.downcase.gsub(/[-?,;.:']/, " ").split(" ")
+
+  stop_str = ""
+  File.open(stop_words_file_name).each_line do |line|
+    stop_str += " " + line
+  end
+  stop_words = stop_str.split(" ")
+
+  hash = {}
+  tab_all.each do |word|
+    hash[word] = tab_all.count(word) if !(stop_words.include?(word))
+  end
+
+  tab_sorted = hash.sort_by { |_, v| v }.reverse[0..number_of_word-1]
+  result = {}
+  tab_sorted.each { |value| result[value[0]] = value[1] }
+
+  return result
 end
+
+#p most_common_words("source-text.txt", "stop_words.txt", 10)
