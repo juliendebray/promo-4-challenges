85c2b42519f4e7b71ef6bbbc34decd17cd5afcf1
Julien Debray <julien.debray.pro@gmail.com>
2014-10-13 11:47:06 +0200

    phone ok

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number.gsub(/\s+|-/,"").match(/^0[1-9][0-9]{8}|^\+33[0-9]{9}/)
+    return true
+  else
+    return false
+  end
 end
