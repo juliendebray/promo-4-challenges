7aa7792828fbd58949a4ebee3c02b43869c0f638
Julien Debray <julien.debray.pro@gmail.com>
2014-10-16 14:52:58 +0200

    animal and tierce

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -2,24 +2,32 @@ class Animal
   def name(name)
     @name = name
     # TODO: find good instruction
+    self
   end
 
   def specie(specie)
     @specie = specie
     # TODO: find good instruction
+    self
   end
 
   def color(color)
     @color = color
     # TODO: find good instruction
+    self
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     # TODO: find good instruction
+    self
   end
 
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
 end
+
+p Animal.new.name("Fox")
+p Animal.new.to_s
+
