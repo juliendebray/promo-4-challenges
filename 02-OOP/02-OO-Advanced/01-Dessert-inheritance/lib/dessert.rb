76c1ae589431018999ce81fb717046fb5b0da91c
Julien Debray <julien.debray.pro@gmail.com>
2014-10-16 11:19:57 +0200

    bicycle and desserts ok

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,34 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
+    @name = name
+    @calories = calories
   end
 
   def healthy?
+    return @calories <= 200
   end
 
   def delicious?
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    if @flavor == "black licorice"
+      res = false
+    else
+      res = super
+    end
+    return res
   end
 end
