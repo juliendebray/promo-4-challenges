ccf6e9aaed14ebc1aa8667a27be62072bcda569f
Julien Debray <julien.debray.pro@gmail.com>
2014-10-27 15:18:09 +0100

    rubucop fixed

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -2,9 +2,9 @@ require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
 100.times do
-  post = Post.new({
+  post = Post.new(
     name: Faker::Company.name,
     url: Faker::Internet.safe_email
-  })
+  )
   post.save
 end
