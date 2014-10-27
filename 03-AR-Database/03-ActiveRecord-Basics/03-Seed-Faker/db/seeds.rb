b82829ec9585d58680f0fe397ddec42d5dda24dc
Julien Debray <julien.debray.pro@gmail.com>
2014-10-27 14:20:15 +0100

    faker

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,10 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+100.times do
+  post = Post.new({
+    name: Faker::Company.name,
+    url: Faker::Internet.safe_email
+  })
+  post.save
+end
