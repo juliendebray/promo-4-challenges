a7786245ce488e88053f6e515a23a07d09aa3b5e
Julien Debray <julien.debray.pro@gmail.com>
2014-10-28 12:23:39 +0100

    active record validations

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates_presence_of :user, :name, :url
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /\Ahttp:\/\/.+\.[a-zA-Z]{2,4}/, message: "invalid url" }
+  # validates :url, format: { with: /.+\..+/, message: "invalid url" }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
   has_many :posts
 
+  before_validation :lower_username
+  before_save :strip_email
   # TODO: Add some validation
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username
+  validates :email, format: { with: /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/, message: "invalid email" }
+  # validates :email, format: {with: /.+@.+/, message: "invalid email"}
+
+  private
+
+  def lower_username
+    self.username = username.downcase unless username.nil?
+  end
+
+  def strip_email
+    self.email = email.strip unless email.nil?
+  end
 end
