3ed4a06c99e08b1bdf8b9c447e1f932a8b72aaf4
Julien Debray <julien.debray.pro@gmail.com>
2014-10-28 11:16:11 +0100

    posts reference to user and db seeds

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,19 @@
 # TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.create(
+    username: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+
+  10.times do
+    Post.create(
+      name: Faker::Commerce.product_name,
+      url: Faker::Internet.url,
+      votes: (0..1000).to_a.sample,
+      user: user
+    )
+  end
+end
+
