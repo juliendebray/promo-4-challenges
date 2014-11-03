37b7156a0afee168a9b7424d2b0c7545ef78bca4
Julien Debray <julien.debray.pro@gmail.com>
2014-11-03 12:24:02 +0100

    jquery common commands

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,45 +8,55 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = $('#email').attr('value');  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
+  $('#email').attr('value','julien@gmail.com');
 
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
+  $('#email-hint').html('<strong>This is my email now</strong>');
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('th').addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
   var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  team_count = $('tr').size() - 1 ;
 
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
+  $('tbody').append('<tr><td>'+(team_count+1)+'</td><td>New Team</td><td>0</td></tr>');
 
 
 
   // Ex 7. Write some code to sum all points given to all teams
   var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
+  $('tbody tr td:nth-child(3)').each(function(i) {
+    sum += parseInt($(this).text());
+  });
 
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
+  $('th').css('background-color', '#DDF4FF');
+
 
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-
+  $( "#results" ).animate({ "top": "+=20px" }, "slow" );
 
 
   // Ex 10. Remove the "Email:" label from the DOM
+  $( 'label' ).remove();
 
 
 
