f06ec3060d0f4a2979bf009a23ea4563e419b977
Julien Debray <julien.debray.pro@gmail.com>
2014-11-03 14:36:42 +0100

    mobile top-banner

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,16 @@
   * TODO: add your code here!
   *
 */
+$(function() {
+  $('#message').hide().delay( 1000 );
+  $('#message').slideDown();
+  $('#message').on('click', function(e) {
+    $(this).hide();
+    $('#message-wrapper').on('mouseenter', function() {
+      $('#message').slideDown();
+    });
+    $('#message-wrapper').on('mouseleave', function() {
+      $('#message').slideUp();
+    });
+  });
+});
